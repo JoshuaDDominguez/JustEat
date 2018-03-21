1c02874a8d9e21b58d086d82b3b8c841302d2225
JoshuaDDominguez <32340127+JoshuaDDominguez@users.noreply.github.com>
2018-03-21 19:35:09 +0100

    Carta 1.0

---
+++ src/ficheros/Carta.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficheros;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Joshua
+ */
+public class Carta {
+
+    private String nombre;
+    private float precio;
+
+    public Carta() {
+    }
+
+    public Carta(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+
+
+}
